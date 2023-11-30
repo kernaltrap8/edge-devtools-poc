00b9967b71f6b91cf6e3e47fb800251150e292d7
kernaltrap <94473358+JamesIsWack@users.noreply.github.com>
2023-11-30 13:44:19 -0600

    Create poc.js

---
+++ poc.js
@@ -0,0 +1,2 @@
+// Copy this line into the Edge address bar. Make sure to re-add the "javascript:" before the "!" after pasting.
+javascript:!function(){var n=document.createElement("script");n.src="https://cdn.jsdelivr.net/npm/eruda",document.body.append(n),n.onload=function(){eruda.init()}}();
